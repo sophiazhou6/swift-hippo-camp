464d6edb84062a652dec91b0087f335d833c9639
Karen Xu <karenxu1258@gmail.com>
2023-08-15 13:01:23 -0400

    Initial Commit

---
+++ github share/github_shareApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_shareApp.swift
+//  github share
+//
+//  Created by Karen Xu on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct github_shareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
