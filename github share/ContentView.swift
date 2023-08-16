ef9a41b21f44f1aaf8d7db72a0f4ada8197f7bbe
Karen Xu <karenxu1258@gmail.com>
2023-08-16 10:52:16 -0400

    added hippo to top

--- github share/ContentView.swift
+++ github share/ContentView.swift
@@ -11,77 +11,79 @@ struct ContentView: View {
     var body: some View {
         
         
-        //        ZStack{
-        //
-        //            Color(.systemPink).ignoresSafeArea()
-        //            Image("rectangle")
-        //                .resizable(resizingMode: .stretch)
-        //.aspectRatio(contentMode: .fill)
-        //
-        //            Text("This is the home page")
         
-   // }
-        
-        NavigationStack{
-            Color.pink
-            ZStack{
+            NavigationStack{
+                HStack {
+                    Image("hippo")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50.0, height: 50.0)
+                        
+                    Text("Hippo Camp")
+                }
+                Color.pink
                 
-            }
-            
-            .toolbar {
                 
-                ToolbarItemGroup(placement: .status) {
+                ZStack{
                     
-                    NavigationLink(destination: ContentView()){
-                        Image("bestHome")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                        Text("     ")
+                }
+                
+                .toolbar {
+                    
+                    ToolbarItemGroup(placement: .status) {
                         
-                        
-                    }
-                    Spacer()
-                    NavigationLink(destination: DietPage()){
-                        Image("diet")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                        Text("     ")
-                    }
-                    Spacer()
-                    NavigationLink(destination: Text("")){
-                        Image("goodExcercise")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                        Text("     ")
-                        
-                    }
-                    Spacer()
-                    NavigationLink(destination: Text("")){
-                        Image("games")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                        Text("     ")
-                        
-                    }
-                    Spacer()
-                    NavigationLink(destination: Text("")){
-                        Image("toDoIcon")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                        Text("     ")
-                        
+                        NavigationLink(destination: ContentView()){
+                            Image("bestHome")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Text("     ")
+                                
+                            
+                        }
+                        Spacer()
+                            
+                        NavigationLink(destination: DietPage()){
+                            Image("diet")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Text("     ")
+                        }
+                        Spacer()
+                        NavigationLink(destination: Text("")){
+                            Image("goodExcercise")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Text("     ")
+                            
+                        }
+                        Spacer()
+                        NavigationLink(destination: Text("")){
+                            Image("games")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Text("     ")
+                            
+                        }
+                        Spacer()
+                        NavigationLink(destination: Text("")){
+                            Image("info")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Text("  ")
+                                
+                            
+                        }
                     }
                 }
+                
             }
-            
         }
-        
         
         
 //        hi my name is Olla
         //this is Julie
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
