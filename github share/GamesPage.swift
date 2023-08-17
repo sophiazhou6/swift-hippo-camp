8d0f99e2e388fe0197e6c1773eca0cf79877d6a4
Julie Hum <julieehum@gmail.com>
2023-08-17 03:20:58 -0400

    i made sudoku.. kind of (can't play)

--- github share/GamesPage.swift
+++ github share/GamesPage.swift
@@ -8,9 +8,121 @@
 import SwiftUI
 
 struct GamesPage: View {
+    var numbers1 = [1, 2, 3, 4]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack{
+            Color(red: 60/255, green: 160/255, blue: 255/255)
+                .ignoresSafeArea()
+            VStack(spacing:0){
+                Text("Problem Solving")
+                HStack{
+                    VStack(alignment: .leading, spacing: 0.0) {
+                        HStack{
+                            Text(" 1 ")
+                                .font(.largeTitle)
+                            Image("linev")
+                                .resizable()
+                                .frame(width: 5.0, height: 50.0)
+                            Text(" 2 ")
+                                .font(.largeTitle)
+                            Image("linev")
+                                .resizable()
+                                .frame(width: 5.0, height: 50.0)
+                            Text(" 3 ")
+                                .font(.largeTitle)
+                            Image("linev")
+                                .resizable()
+                                .frame(width: 5.0, height: 50.0)
+                            Text(" 4")
+                                .font(.largeTitle)
+                        }
+                        HStack{
+                            Image("lineh")
+                                .resizable()
+                                .frame(width: 200.0, height: 5.0)
+                        }
+                        HStack{
+                            Text(" 1 ")
+                                .font(.largeTitle)
+                            Image("linev")
+                                .resizable()
+                                .frame(width: 5.0, height: 50.0)
+                            Text(" 2 ")
+                                .font(.largeTitle)
+                            Image("linev")
+                                .resizable()
+                                .frame(width: 5.0, height: 50.0)
+                            Text(" 3 ")
+                                .font(.largeTitle)
+                            Image("linev")
+                                .resizable()
+                                .frame(width: 5.0, height: 50.0)
+                            Text(" 4")
+                                .font(.largeTitle)
+                        }
+                        HStack{
+                            Image("lineh")
+                                .resizable()
+                                .frame(width: 200.0, height: 5.0)
+                        }
+                        HStack{
+                            Text(" 1 ")
+                                .font(.largeTitle)
+                            Image("linev")
+                                .resizable()
+                                .frame(width: 5.0, height: 50.0)
+                            Text(" 2 ")
+                                .font(.largeTitle)
+                            Image("linev")
+                                .resizable()
+                                .frame(width: 5.0, height: 50.0)
+                            Text(" 3 ")
+                                .font(.largeTitle)
+                            Image("linev")
+                                .resizable()
+                                .frame(width: 5.0, height: 50.0)
+                            Text(" 4")
+                                .font(.largeTitle)
+                        }
+                        HStack{
+                            Image("lineh")
+                                .resizable()
+                                .frame(width: 200.0, height: 5.0)
+                        }
+                        HStack{
+                            Text(" 1 ")
+                                .font(.largeTitle)
+                            Image("linev")
+                                .resizable()
+                                .frame(width: 5.0, height: 50.0)
+                            Text(" 2 ")
+                                .font(.largeTitle)
+                            Image("linev")
+                                .resizable()
+                                .frame(width: 5.0, height: 50.0)
+                            Text(" 3 ")
+                                .font(.largeTitle)
+                            Image("linev")
+                                .resizable()
+                                .frame(width: 5.0, height: 50.0)
+                            Text(" 4")
+                                .font(.largeTitle)
+                        }
+                        
+                    }
+                    .padding()
+                    .background(Rectangle() .foregroundColor(.white))
+                    .cornerRadius(15)
+                    .shadow(radius:15)
+                    .padding()
+                    
+                }
+            }
+   
+        }
     }
+            
 }
 
 struct GamesPage_Previews: PreviewProvider {
