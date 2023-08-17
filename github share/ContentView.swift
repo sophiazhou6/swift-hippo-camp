533b7c3bddb841b7838a832602231491c04255c8
Karen Xu <karenxu1258@gmail.com>
2023-08-16 22:39:54 -0400

    added some info to info pg

--- github share/ContentView.swift
+++ github share/ContentView.swift
@@ -8,100 +8,101 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    
     var body: some View {
         
-        // refer to newtoolbar
+//         refer to newtoolbar
+//
+//         we can comment out this entire thing
+                    NavigationStack{
+                            HStack {
         
-        // we can comment out this entire thing
-            NavigationStack{
-                    HStack {
-                        
-                        Image("hippo")
-                        
-                            .resizable(resizingMode: .stretch)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 50.0, height: 50.0)
-                        Spacer()
-                        
-                        Text("Hippo Camp")
-                            .font(.title)
-                        Spacer()
-                        Spacer()
-                        
-                        
-                    }
-                    Color.pink
-                    //Color(red: 255/255, green: 165/255, blue: 171/255)
-                    Text("hi")
-                    
-                    
-                    ZStack{
-                        Text("test")
-                    }
-                    .navigationBarBackButtonHidden(true)
-                    .navigationBarTitleDisplayMode(.inline)
-                    .toolbarBackground(.pink, for: .navigationBar)
-                    .toolbarBackground(.visible, for: .navigationBar)
-                    .toolbar {
-                        
-                        ToolbarItemGroup(placement: .status) {
-                            
-                            NavigationLink(destination: ContentView()){
-                                Image("bestHome")
-                                    .resizable()
-                                    .frame(width: 30, height: 30)
-                                Text("     ")
+                                Image("hippo")
+        
+                                    .resizable(resizingMode: .stretch)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 50.0, height: 50.0)
+                                Spacer()
+        
+                                Text("Hippo Camp")
+                                    .font(.title)
+                                Spacer()
+                                Spacer()
+        
+        
                             }
-                            Spacer()
-                            
-                            NavigationLink(destination: DietPage()){
-                                Image("diet")
-                                    .resizable()
-                                    .frame(width: 30, height: 30)
-                                Text("     ")
+                            Color.pink
+                            //Color(red: 255/255, green: 165/255, blue: 171/255)
+                            Text("hi")
+        
+        
+                            ZStack{
+                                Text("test")
                             }
-                            Spacer()
-                            NavigationLink(destination: Text("")){
-                                Image("goodExcercise")
-                                    .resizable()
-                                    .frame(width: 30, height: 30)
-                                Text("     ")
-                                
+                            .navigationBarBackButtonHidden(true)
+                            .navigationBarTitleDisplayMode(.inline)
+                            .toolbarBackground(.pink, for: .navigationBar)
+                            .toolbarBackground(.visible, for: .navigationBar)
+                            .toolbar {
+        
+                                ToolbarItemGroup(placement: .status) {
+        
+                                    NavigationLink(destination: ContentView()){
+                                        Image("bestHome")
+                                            .resizable()
+                                            .frame(width: 30, height: 30)
+                                        Text("     ")
+                                    }
+                                    Spacer()
+        
+                                    NavigationLink(destination: DietPage()){
+                                        Image("diet")
+                                            .resizable()
+                                            .frame(width: 30, height: 30)
+                                        Text("     ")
+                                    }
+                                    Spacer()
+                                    NavigationLink(destination: Text("")){
+                                        Image("goodExcercise")
+                                            .resizable()
+                                            .frame(width: 30, height: 30)
+                                        Text("     ")
+        
+                                    }
+                                    Spacer()
+                                    NavigationLink(destination: Text("")){
+                                        Image("games")
+                                            .resizable()
+                                            .frame(width: 30, height: 30)
+                                        Text("     ")
+        
+                                    }
+                                    Spacer()
+                                    NavigationLink(destination: Text("")){
+                                        Image("info")
+                                            .resizable()
+                                            .frame(width: 30, height: 30)
+                                        Text("  ")
+        
+        
+                                    }
+                                }
                             }
-                            Spacer()
-                            NavigationLink(destination: Text("")){
-                                Image("games")
-                                    .resizable()
-                                    .frame(width: 30, height: 30)
-                                Text("     ")
-                                
-                            }
-                            Spacer()
-                            NavigationLink(destination: Text("")){
-                                Image("info")
-                                    .resizable()
-                                    .frame(width: 30, height: 30)
-                                Text("  ")
-                                
-                                
-                            }
-                        }
-                    }
-                    //.navigationBarTitle("")
-                    .navigationBarBackButtonHidden(true)
-                    .navigationBarHidden(true)
+                            //.navigationBarTitle("")
+                            .navigationBarBackButtonHidden(true)
+                            .navigationBarHidden(true)
+                }
+            }
+        
+        
+//                hi my name is Olla
+//        this is Julie
+    }
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
         }
     }
-        
-        
-//        hi my name is Olla
-        //this is Julie
-    }
 
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
--- github share/InfoPage.swift
+++ github share/InfoPage.swift
@@ -9,12 +9,39 @@ import SwiftUI
 
 struct InfoPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Hippo Camp's Purpose")
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+            
+            Text("Our app focuses on the prevention of Alzheimer’s, which will be done through improving mental health, improving physical health, and other beneficial activities (meditation, yoga, music).")
+                .multilineTextAlignment(.center)
+                .padding()
+                
+            Text("Below you can find some further information on Alzheimer's:")
+                .fontWeight(.bold)
+                
+                .font(.title2)
+                .padding()
+            
+            Text("Symptoms of Alzheimer's:")
+                .fontWeight(.bold)
+                
+            Text("Memory loss")
+                
+            Text("Bad judgment")
+               
+            Text("Taking longer to complete everyday tasks")
+            Text("Repeating questions")
+            
+            
+        }
     }
-}
-
-struct InfoPage_Previews: PreviewProvider {
-    static var previews: some View {
-        InfoPage()
+    
+    struct InfoPage_Previews: PreviewProvider {
+        static var previews: some View {
+            InfoPage()
+        }
     }
 }
